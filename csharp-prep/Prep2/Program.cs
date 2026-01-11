250cbca2969faa611440bb3f8c9ce2051bc75d77
SovereignEternal <tannerlyche@gmail.com>
2026-01-10 17:12:40 -0700

    Prep Assignment #2 Submission

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        string letterGrade = "";
+        Console.Write("What was your grade percentage in this class? Do not include decimal points. ");
+        string gradeInput = Console.ReadLine();
+        int grade = int.Parse(gradeInput);
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        
+        Console.WriteLine($"Your grade in this class was: {letterGrade}.");
+
+        if (letterGrade == "A" || letterGrade == "B" || letterGrade == "C")
+        {
+            Console.WriteLine("Congratulations on passing the class!");
+        } 
+        else
+        {
+            Console.WriteLine("Don't worry, you'll do better next time!");
+        }
+    }   
 }
