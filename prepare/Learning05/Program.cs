06c94489e465d2accbb9ac4b75e845cbede3a1aa
SovereignEternal <tannerlyche@gmail.com>
2026-01-17 16:38:17 -0700

    Initial project submission for C# Prep 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        int birthYear = 2000;
+
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favoriteNumber = PromptFavoriteNumber();
+        PromptUserBirthYear(out birthYear);
+        int squaredNumber = SquareNumber(favoriteNumber);
+        DisplayResult(userName, squaredNumber, birthYear);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please input your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptFavoriteNumber()
+    {
+        Console.Write("What is your favorite number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+        return favoriteNumber;
+    }
+
+    static void PromptUserBirthYear(out int birthYear)
+    {
+        Console.Write("Enter your birth year: ");
+        birthYear = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int factor)
+    {
+        int product = factor * factor;
+        return product;
+    }
+
+    static void DisplayResult(string userName, int squaredNumber, int birthYear)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}. ");
+        int age = 2026 - birthYear;
+        Console.WriteLine($"{userName}, you will turn {age} this year. ");
     }
 }
